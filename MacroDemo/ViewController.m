a40c6b08394966297db019e1abe03445fb5aae9c
hewenjie <123610105@qq.com>
2020-01-17 17:46:25 +0800

    ..

--- MacroDemo/ViewController.m
+++ MacroDemo/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+#define SSSV(s1, s2, s3, v) s1 = length * width; s2 = length * height; s3 = width * height; v = width * length * height;
+
 @interface ViewController ()
 
 @end
@@ -17,7 +19,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    int length = 3, width = 4, height = 5, sa, sb, sc, vv;
+    SSSV(sa, sb, sc, vv);
+    printf("sa=%d, sb=%d, sc=%d, vv=%d\n", sa, sb, sc, vv);
+    
+    [self book];
 }
 
+- (void)book
+{
+    
+}
+
+- (void)fly
+{
+    
+}
 
 @end
